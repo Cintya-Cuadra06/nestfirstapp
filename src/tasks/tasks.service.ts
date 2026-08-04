6cb02efca3421cb7d5954d59b7bfa71ecbbd3e5e
Cintya Cuadra Salazar <cintyacuadrasalazar@gmail.com>
2026-08-04 16:42:06 -0600

    feat: implement tasks service and controller

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@nestjs/common';
-
+import { Task, TaskStatus } from './task.entity';
+import { v4 } from 'uuid'
 @Injectable()
 export class TasksService {
 
-    getAllTasks(){
-        return [{
-            id: 1,
+
+  private tasks: Task[] = [{
+            id: '1',
             title: 'first task',
-            description: 'some task'
+            description: 'some task',
+            status: TaskStatus.PENDING,
         }]
+
+    getAllTasks(){
+        return this.tasks;
     }
-    
-    createTask(){}
+
+    createTask(title: string, description: string, ){
+        const task = {
+            id: v4(),
+            title,
+            description,
+            status: TaskStatus.PENDING
+        };
+        this.tasks.push(task); 
+
+        return task;
+    }
     updateTask(){}
     deleteTask(){}
 
