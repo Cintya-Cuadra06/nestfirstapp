39837119df3bb8d54b99ba94470c56a904a89efa
Cintya Cuadra Salazar <cintyacuadrasalazar@gmail.com>
2026-08-04 16:37:29 -0600

    Functions update

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+
+    getAllTasks(){
+        return [{
+            id: 1,
+            title: 'first task',
+            description: 'some task'
+        }]
+    }
+    
+    createTask(){}
+    updateTask(){}
+    deleteTask(){}
+
+}
