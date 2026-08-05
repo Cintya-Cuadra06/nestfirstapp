90ebb5a653f399f92fd605c618e9f84e338bb6bb
Cintya Cuadra Salazar <cintyacuadrasalazar@gmail.com>
2026-08-05 08:40:33 -0600

    implementacion de CreateDto en el controlador

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,21 +1,21 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import {TasksService} from './tasks.service'
-
+import { CreateTaskDto } from './dto/task.dto'; 
 @Controller('tasks')
 export class TasksController {
-
+//inyecta el servicio para poder leer la logica de las tareas
     constructor(private tasksService: TasksService){}
 
+//devuelve todas las tareas registradas 
     @Get()
     getAllTasks(){
        return this.tasksService.getAllTasks();
     }
-
+//nuevo controller
+//recibe una nueva peticion de crear una nueva tarea 
     @Post()
 
-    createTask(@Body() newTask: any){
-        console.log(newTask);
-        return'Guardando'
-        //this.tasksService.createTask();
+    createTask(@Body() newTask: CreateTaskDto){
+     return this.tasksService.createTask(newTask.title, newTask.description);
     }
 }
