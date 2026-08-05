fe029a81e7f2fee55cb7f594deaf966ee6e18e98
Cintya Cuadra Salazar <cintyacuadrasalazar@gmail.com>
2026-08-05 09:48:55 -0600

    se incluye las actualizaciones de tareas

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.entity';
 import { v4 } from 'uuid'
+import { UpdatedTaskDto } from './dto/task.dto';
 @Injectable()
 export class TasksService {
 
-
+//Simula una DB real mientras no hay conexion a una 
   private tasks: Task[] = [{
             id: '1',
             title: 'first task',
             description: 'some task',
             status: TaskStatus.PENDING,
         }]
-
+//devuelve todas las tareas almacenadas
     getAllTasks(){
         return this.tasks;
     }
-
+//crea una nueva tarea con base en esos parametros, con un id totalmente unico
     createTask(title: string, description: string, ){
         const task = {
             id: v4(),
@@ -27,7 +28,22 @@ export class TasksService {
 
         return task;
     }
-    updateTask(){}
-    deleteTask(){}
+
+    getTaskById(id: string): Task {
+        return this.tasks.find(task => task.id === id)!
+    }
+    updateTask(id: string, updatedFields: UpdatedTaskDto): Task{
+        const task = this.getTaskById(id)
+        const newTask = Object.assign(task, updatedFields)
+        this.tasks = this.tasks.map(task => task.id === id ? newTask : task )
+        return newTask;
+    }
+
+
+
+
+    deleteTask(id: string){
+       this.tasks = this.tasks.filter(task => task.id !== id)
+    }
 
 }
